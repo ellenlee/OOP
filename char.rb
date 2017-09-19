a8f330a041e5a2d0e680851f4718aeaac054134e
arch <arch@alphacamp.tw>
2017-09-19 10:12:43 +0800

    rpg version

--- char.rb
+++ char.rb
@@ -1,47 +1,46 @@
 class Character
 
-	def initialize name
-		@name = name
-		@hp = 40
-		@speed = 3
+  def initialize name
+    @name = name
+    @hp = 40
+    @speed = 1
   end
 
   def get_name
     @name
-	end
+  end
 
-	def move
-		return @speed
-	end
+  def move
+    return @speed
+  end
 
-	def  still_alive
-		return !(@hp <= 0)
-	end
+  def  still_alive
+    return !(@hp <= 0)
+  end
 
 end
 
-@erica = Character.new 'Erica'
+puts "請問你的冒險者名稱？"
+charname = gets
+charname.delete!("\n")
+
+@yourchar = Character.new charname
 
 loc = 0
 count = 0
 
-while( loc < 10 )
-  puts 'Erica run for ' + @erica.move.to_s
-  loc += @erica.move
+while( loc < 21 && count < 7)
+  puts "你的冒險者 #{@yourchar.get_name} 往前移動 #{@yourchar.move} 步" 
+  loc += @yourchar.move
   count += 1
 end
 
-puts "恭喜 #{@erica.get_name} 花了 #{count} 步抵達迷霧山莊了"
+total = @yourchar.move * count
 
-loc = 0
-count = 0
-slice = 0
-
-while( loc < 10 )
-  puts 'Erica run for ' + @erica.move.to_s
-  loc += @erica.move
-  count += 1
+if count >= 7
+  puts "你跑了 #{count} 小時，跑太慢了！糧食都已經吃光了！你需要回去市鎮補貨再來過！"
+elsif loc > 24
+  puts "你跑了 #{total} 步跑過了頭，你錯過了該去的地方，直接抵達了下一個城鎮！"
+else
+  puts "#{@yourchar.get_name} 花了 #{count} 回合抵達迷霧森林！準備迎接下一個挑戰吧！"
 end
-
-puts "恭喜 #{@erica.get_name} 花了 #{count} 步抵達迷霧山莊了"
-
