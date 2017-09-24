e2b4d33fd0e6ef8deb5a30b0e862d991c666d87f
Ellen Lee <leechinghui.tw@gmail.com>
2017-09-24 17:36:12 +0800

    refactory

--- character.rb
+++ character.rb
@@ -1,21 +1,31 @@
 class Character
+  POINTS_PER_LEVEL = 10
+
   attr_accessor :name
 
-  def initialize(name)
-    @name = name
+  def initialize
+    set_name
     @stamina = 7
-    puts "你召喚了一個名叫 #{@name} 的勇者！(體力值: #{@stamina})"
+    puts "#{@name} 的體力值: #{@stamina}"
+  end
+
+  def set_name
+    puts "請問你的勇者名稱？"
+    @name = gets.chomp
+    puts "你召喚了一個名叫 #{@name} 的勇者！"
   end
 
   def use_stamina
     @stamina -= 1
+    stamina_left # 在 class 內部呼叫私有方法 stamina_left
+  end
+
+  private
+
+  def stamina_left
+    puts "#{@name} 還擁有 #{@stamina} 的體力"
   end
 end
 
-puts "請問你的勇者名稱？"
-hero_name = gets.chomp
-
-hero = Character.new(hero_name)
+hero = Character.new
 puts "你的勇者 #{hero.name} 準備出發去迷霧森林了！"
-
-hero.use_stamina
