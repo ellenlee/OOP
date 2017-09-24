48c246ab58c29c0911885abbc96ea22c528ca2b5
Ellen Lee <leechinghui.tw@gmail.com>
2017-09-24 19:51:06 +0800

    add count

--- character.rb
+++ character.rb
@@ -1,12 +1,18 @@
 class Character
   POINTS_PER_LEVEL = 10
 
-  attr_accessor :name
+  @@character_count = 0
+
+  attr_accessor :name, :strength, :speed
 
   def initialize
     set_name
+    @level = 1
     @stamina = 7
-    puts "#{@name} 的體力值: #{@stamina}"
+
+    set_strength_and_speed
+
+    @@character_count += 1
   end
 
   def set_name
@@ -15,9 +21,32 @@ class Character
     puts "你召喚了一個名叫 #{@name} 的勇者！"
   end
 
+  def set_strength_and_speed
+    puts "請輸入攻擊力。（剩餘點數：#{POINTS_PER_LEVEL}）"
+    self.strength = gets.chomp.to_i
+
+    puts "請輸入速度。（剩餘點數：#{POINTS_PER_LEVEL - @strength}）"
+    self.speed = gets.chomp.to_i
+
+    validate_distribution_of_points
+  end
+
   def use_stamina
     @stamina -= 1
     stamina_left # 在 class 內部呼叫私有方法 stamina_left
+  end
+
+  def validate_distribution_of_points
+    if (@strength + @speed) > ( POINTS_PER_LEVEL * @level ) # 速度與力量合計不能超過等級允許的數值
+      puts "數值設定有誤，請再設定一次!!"
+      set_strength_and_speed
+    else
+      puts "設定完成！你的勇者 #{@name} 攻擊力：#{@strength}, 速度：#{@speed}"
+    end
+  end
+
+  def self.count
+    @@character_count
   end
 
   private
@@ -29,3 +58,5 @@ end
 
 hero = Character.new
 puts "你的勇者 #{hero.name} 準備出發去迷霧森林了！"
+
+puts "目前遊戲中有 #{Character.count} 個勇者"
