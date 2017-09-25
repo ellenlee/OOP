2c3b28e1b809edabcc9cf55527d3e6f8229f3f0f
Ellen Lee <leechinghui.tw@gmail.com>
2017-09-25 10:44:08 +0800

    add constant 'JOB'

--- inheritance/your_character.rb
+++ inheritance/your_character.rb
@@ -1,15 +1,14 @@
-# puts "load character.rb"
 require './character.rb'
 
-class Warrior
+class Warrior < Character
+  JOB = "Warrior"
 end
 
 class Mage < Character
+  JOB = "Mage"
 end
 
-puts "start test"
-
-Mage.new
-
-puts Mage.count
-# puts Character.count
+# puts "start test"
+#
+# hero1 = Warrior.new
+# hero2 = Mage.new
